fa6678cc99fff4ab96de48c69e327f856fcf4e81
Swateja Patil <swatejapatil3@gmail.com>
2026-01-11 22:43:19 +0530

    fix: add postinstall script for prisma generate

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-// require('dotenv').config();
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
