0965f6142c1bdeb63c52aa908d51031a6220197d
Swateja Patil <swatejapatil3@gmail.com>
2026-01-11 22:55:41 +0530

    Use production backend URL

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import axios from 'axios'
 
-if (import.meta.env.VITE_API_BASE_URL) {
-  axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+if (import.meta.env.VITE_API_URL) {
+  axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 }
 
 createRoot(document.getElementById('root')).render(
